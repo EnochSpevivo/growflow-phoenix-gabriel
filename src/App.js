850bf2c7ab5e5960060fff898ce6b2c3ab319669
gabriel <akingdebased@gmail.com>
2020-07-29 14:45:16 -0700

    update readme, add .env.sample...
    * update packages
    * begin implementing http requests

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useEffect, Fragment } from "react";
+import axios from "axios";
+import querystring from "querystring";
+
+import Navbar from "./components/Navbar";
+
+import "bulma/css/bulma.css";
 import "typeface-montserrat";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    axios
+      .get(
+        `${process.env.REACT_APP_API_URL}`,
+        querystring.stringify({
+          ApiUser: process.env.REACT_APP_API_USER,
+          ApiKey: process.env.REACT_APP_API_KEY,
+        })
+      )
+      .then((payload) => {
+        console.log("payload", payload);
+      })
+      .catch((error) => {
+        console.log("some real bullshit", error);
+      });
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Navbar></Navbar>
+    </Fragment>
   );
 }
 
