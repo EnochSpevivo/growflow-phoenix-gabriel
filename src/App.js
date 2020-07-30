d479010121c4071ef80e954ef3ee17d6c37840b1
gabriel <akingdebased@gmail.com>
2020-07-29 17:55:35 -0700

    implement sidenav bar

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Navbar from "./components/Navbar";
 
 import "bulma/css/bulma.css";
 import "typeface-montserrat";
-import "./App.css";
+import "./App.scss";
 
 function App() {
   useEffect(() => {
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,48 @@
-import React, { useEffect, Fragment } from "react";
+import React, { useEffect, useRef, useState, Fragment } from "react";
 import { ProSidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
 
 import "react-pro-sidebar/dist/css/styles.css";
+import "styles/Navbar.scss";
 
-function App() {
-  useEffect(() => {}, []);
+function App(ref) {
+  const [isToggled, setIsToggled] = useState(false);
 
   return (
-    <ProSidebar breakPoint={"sm"}>
-      <Menu iconShape="square">
-        {/* TODO: Icons? */}
-        <MenuItem>Dashboard</MenuItem>
-        <SubMenu title="Components">
-          <MenuItem>Component 1</MenuItem>
-          <MenuItem>Component 2</MenuItem>
-        </SubMenu>
-      </Menu>
-    </ProSidebar>
+    <Fragment>
+      <div className={`navbar__toggle-container ${isToggled ? "hide" : ""}`}>
+        <span
+          className="navbar__toggle"
+          onClick={setIsToggled.bind(this, !isToggled)}
+        >
+          <svg
+            stroke="currentColor"
+            fill="currentColor"
+            strokeWidth="0"
+            viewBox="0 0 448 512"
+            height="1em"
+            width="1em"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"></path>
+          </svg>
+        </span>
+      </div>
+
+      <ProSidebar
+        breakPoint={"sm"}
+        toggled={isToggled}
+        onToggle={() => setIsToggled(false)}
+      >
+        <Menu iconShape="square">
+          {/* TODO: Icons? */}
+          <MenuItem>Dashboard</MenuItem>
+          <SubMenu title="Components">
+            <MenuItem>Component 1</MenuItem>
+            <MenuItem>Component 2</MenuItem>
+          </SubMenu>
+        </Menu>
+      </ProSidebar>
+    </Fragment>
   );
 }
 
